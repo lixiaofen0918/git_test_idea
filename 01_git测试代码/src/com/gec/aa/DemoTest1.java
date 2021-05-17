30deaffd0500adfdec72420f6a82a38ee1b7170a
xiaofen <xiaofen@qq.com>
2021-05-17 16:26:51 +0800

    第一次提交代码

---
+++ 01_git测试代码/src/com/gec/aa/DemoTest1.java
@@ -0,0 +1,13 @@
+package com.gec.aa;
+
+public class DemoTest1 {
+
+    public static void main(String[] args) {
+        System.out.println("111111111111");
+        System.out.println("111111111111");
+        System.out.println("111111111111");
+        System.out.println("111111111111");
+        System.out.println("111111111111");
+        System.out.println("111111111111");
+    }
+}
